1f65e4af230224b4b766fb6dfcc9b8f8e4c24363
priteshbhole <priteshbhole11@gmail.com>
2018-09-06 15:32:49 +0100

    finished till canAddFish problem

--- src/AMS.kt
+++ src/AMS.kt
@@ -4,7 +4,11 @@ fun main(args: Array<String>) {
 
     println("hello ..  ${args[0]}")
     feedTheFish()
-    getFortune()
+//    getFortune()
+    println(canAddFish(10.0, listOf(3, 3, 3)))
+    println(canAddFish(8.0, listOf(2, 2, 2), hasDecorations = false))
+    println(canAddFish(9.0, listOf(1, 1, 3), 3))
+    println(canAddFish(10.0, listOf(), 7, true))
 }
 
 fun feedTheFish() {
@@ -33,6 +37,19 @@ fun fishFood(day: String): String {
     }
 }
 
+fun canAddFish(tankSize: Double,
+               currentFish: List<Int>,
+               fishSize: Int = 2,
+               hasDecorations: Boolean = true): Boolean {
+    return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
+}
+
+
+fun shouldChangeWater(temperature: Int = 22,
+                      dirty: Int = 20,
+                      day: String) {
+
+}
 
 
 fun getFortune() {
