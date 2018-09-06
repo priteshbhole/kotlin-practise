ffaad1bd809990568d0b940ca8c1d1c8765284b4
priteshbhole <priteshbhole11@gmail.com>
2018-09-06 14:33:26 +0100

    function and loops

---
+++ src/AMS.kt
@@ -0,0 +1,67 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    println("hello ..  ${args[0]}")
+    feedTheFish()
+    getFortune()
+}
+
+fun feedTheFish() {
+    val day = randomDay();
+    val food = fishFood(day)
+    println("today is $day and fish eats  $food")
+}
+
+fun randomDay(): String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
+}
+
+fun fishFood(day: String): String {
+    var food = "fasting"
+    return when (day) {
+        "Monday" -> "flakes"
+        "Tuesday" -> "pellets"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "lettuce"
+        "Saturday" -> "plankton"
+        "Sunday" -> "mosquitoes"
+        else -> food
+
+    }
+}
+
+
+
+fun getFortune() {
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortune(getBirthday())
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break;
+    }
+}
+
+fun getBirthday(): Int {
+    print("\nEnter your birthday: ")
+    return readLine()?.toIntOrNull() ?: 1
+}
+
+fun getFortune(birthday: Int): String {
+    val fortunes = listOf("You will have a great day!",
+            "Things will go well for you today.",
+            "Enjoy a wonderful day of success.",
+            "Be humble and all will turn out well.",
+            "Today is a good day for exercising restraint.",
+            "Take it easy and enjoy life!",
+            "Treasure your friends, because they are your greatest fortune.")
+    val index = when (birthday) {
+        in 1..7 -> 4
+        28, 31 -> 2
+        else -> birthday.rem(fortunes.size)
+    }
+    return fortunes[index]
+}
+
